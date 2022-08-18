d7cd6b5e2b976119614845178fcab0335a7245d6
Oskar Skog <oskar@oskog97.com>
2022-08-18 22:09:42 +0300

    UTF-8 output was bodged, tidy up hack.cc

--- winsup/cygwin/hack.cc
+++ winsup/cygwin/hack.cc
@@ -16,27 +16,17 @@ bool hack_debug_enabled;
 // debug log file
 static HANDLE debug_log;
 
-// Sanitize filenames (not paths), modifies supplied string.
-static void sanitize(char *s)
-{
-    for (; s[0]; s++) {
-        if (s[0] < 32 || s[0] > 126)
-            s[0] = '_';
-        switch (s[0]) {
-            case '<':
-            case '>':
-            case ':':
-            case '"':
-            case '/':
-            case '\\':
-            case '|':
-            case '?':
-            case '*':
-            case '^':
-                s[0] = '_';
-        }
-    }
-}
+
+// Function prototypes for various internal functions
+static void sanitize(char *s);              // Sanitize filename
+static void writestr(const char *str);      // Write string to debug_log
+static void push_utf8(
+    char *_dst,
+    size_t dst_size,
+    size_t &dst_index,
+    int codepoint
+);
+
 
 // Open the log file
 // This function is called immediately before main() so all POSIX functions
@@ -85,23 +75,37 @@ void hack_init(int argc, const char * const * argv)
     hack_print("Test %s\r\n", test);
 }
 
-// Write NUL terminated string to debug_log, does not flush
-static void writestr(const char *str)
+// Sanitize filenames (not paths), modifies supplied string.
+static void sanitize(char *s)
 {
-    DWORD bytes_written = 0;
-    DWORD bytes_to_write = strlen(str);
-    DWORD tmp;
-    while (bytes_written < bytes_to_write) {
-        WriteFile(
-            debug_log,
-            str+bytes_written,
-            bytes_to_write-bytes_written,
-            &tmp,
-            NULL
-        );
-        bytes_written += tmp;
+    for (; s[0]; s++) {
+        if (s[0] < 32 || s[0] > 126)
+            s[0] = '_';
+        switch (s[0]) {
+            case '<':
+            case '>':
+            case ':':
+            case '"':
+            case '/':
+            case '\\':
+            case '|':
+            case '?':
+            case '*':
+            case '^':
+                s[0] = '_';
+        }
     }
 }
+
+
+// Called by do_exit in winsup/cygwin/dcrt0.cc
+// stdio cleanup may have occured
+void hack_end()
+{
+    CloseHandle(debug_log);
+}
+
+
 
 // Basically a printf(3) that writes to debug_log without calling any unix
 // syscalls.  Only uses stdarg.h macros and vsnprintf(3)
@@ -120,51 +124,29 @@ void hack_print(const char *format, ...)
     FlushFileBuffers(debug_log);
 }
 
-// Called by do_exit in winsup/cygwin/dcrt0.cc
-// stdio cleanup may have occured
-void hack_end()
+// Write NUL terminated string to debug_log, does not flush
+static void writestr(const char *str)
 {
-    CloseHandle(debug_log);
-}
-
-static void push_utf8(
-    char *_dst,
-    size_t dst_size,
-    size_t &dst_index,
-    int codepoint)
-{
-    // Need to be unsigned for math to not be crazy, but I'm too lazy
-    // to write unsigned char all the time
-    unsigned char *dst = (unsigned char *) _dst;
-    // -1 to preserve a single byte for a trailing NUL
-    size_t freespace = dst_size - dst_index - 1;
-    if (codepoint == 0) {
-        if (freespace >= 0)
-            dst[dst_index++] = 0;
-    } else if (codepoint < 1<<7) {
-        if (freespace >= 1) {
-            dst[dst_index++] = codepoint;
-        }
-    } else if (codepoint < 1<<11) {
-        if (freespace >= 2) {
-            dst[dst_index++] = 0xc0 | (0x1f & (codepoint >> 6));
-            dst[dst_index++] = 0x80 | (0x3f & codepoint);
-        }
-    } else if (codepoint < 1<<16) {
-        if (freespace >= 3) {
-            dst[dst_index++] = 0xd0 | (0x0f & (codepoint >> 12));
-            dst[dst_index++] = 0x80 | (0x3f & (codepoint >> 6));
-            dst[dst_index++] = 0x80 | (0x3f & codepoint);
-        }
-    } else {
-        if (freespace >= 4) {
-            dst[dst_index++] = 0xe0 | (0x07 & (codepoint >> 18));
-            dst[dst_index++] = 0x80 | (0x3f & (codepoint >> 12));
-            dst[dst_index++] = 0x80 | (0x3f & (codepoint >> 6));
-            dst[dst_index++] = 0x80 | (0x3f & codepoint);
-        }
+    DWORD bytes_written = 0;
+    DWORD bytes_to_write = strlen(str);
+    DWORD tmp;
+    while (bytes_written < bytes_to_write) {
+        WriteFile(
+            debug_log,
+            str+bytes_written,
+            bytes_to_write-bytes_written,
+            &tmp,
+            NULL
+        );
+        bytes_written += tmp;
     }
 }
+
+
+
+
+
+// Unicode conversion
 
 void hack_utf16_to_utf8(char *dst, size_t dst_size, LPWSTR src)
 {
@@ -193,3 +175,42 @@ void hack_utf16_to_utf8(char *dst, size_t dst_size, LPWSTR src)
     dst[dst_size - 1] = 0;
 }
 
+
+static void push_utf8(
+    char *_dst,
+    size_t dst_size,
+    size_t &dst_index,
+    int codepoint)
+{
+    // Need to be unsigned for math to not be crazy, but I'm too lazy
+    // to write unsigned char all the time
+    unsigned char *dst = (unsigned char *) _dst;
+    // -1 to preserve a single byte for a trailing NUL
+    size_t freespace = dst_size - dst_index - 1;
+    if (codepoint == 0) {
+        if (freespace >= 0)
+            dst[dst_index++] = 0;
+    } else if (codepoint < 1<<7) {
+        if (freespace >= 1) {
+            dst[dst_index++] = codepoint;
+        }
+    } else if (codepoint < 1<<11) {
+        if (freespace >= 2) {
+            dst[dst_index++] = 0xc0 | (0x1f & (codepoint >> 6));
+            dst[dst_index++] = 0x80 | (0x3f & codepoint);
+        }
+    } else if (codepoint < 1<<16) {
+        if (freespace >= 3) {
+            dst[dst_index++] = 0xe0 | (0x0f & (codepoint >> 12));
+            dst[dst_index++] = 0x80 | (0x3f & (codepoint >> 6));
+            dst[dst_index++] = 0x80 | (0x3f & codepoint);
+        }
+    } else {
+        if (freespace >= 4) {
+            dst[dst_index++] = 0xf0 | (0x07 & (codepoint >> 18));
+            dst[dst_index++] = 0x80 | (0x3f & (codepoint >> 12));
+            dst[dst_index++] = 0x80 | (0x3f & (codepoint >> 6));
+            dst[dst_index++] = 0x80 | (0x3f & codepoint);
+        }
+    }
+}
