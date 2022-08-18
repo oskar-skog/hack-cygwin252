e924d50b9c9e21f559e5aa46c2495e3250d51abe
Oskar Skog <oskar@oskog97.com>
2022-08-18 13:52:18 +0300

    Bypass -Werror for unused variables

--- winsup/cygwin/hack.cc
+++ winsup/cygwin/hack.cc
@@ -13,6 +13,10 @@ void hack_init(int argc, const char * const * argv)
     hack_debug_enabled = true;
 }
 
+static void no_op(...)
+{
+}
+
 void hack_print(const char *format, ...)
 {
     if (!hack_debug_enabled)
@@ -22,4 +26,6 @@ void hack_print(const char *format, ...)
     va_start(args, format);
     bool truncated = vsnprintf(buf, MAXLEN, format, args) > MAXLEN;
     va_end(args);
+    // -Werror for unused variables
+    no_op(truncated);
 }
