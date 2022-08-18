7dd210538cb9a1a85ce770b4727c3256c62bdd19
Oskar Skog <oskar@oskog97.com>
2022-08-18 13:48:35 +0300

    Trying to implement hack_print

--- winsup/cygwin/hack.cc
+++ winsup/cygwin/hack.cc
@@ -1,9 +1,25 @@
-#include "hack.h"
-
+#include <stdarg.h>
 #include <stdio.h>
+
+#include "hack.h"
+#define MAXLEN 1024
+
+bool hack_debug_enabled = false;
 
 void hack_init(int argc, const char * const * argv)
 {
     // Proof of concept
     // fputs("Hello world!\n", stderr);
+    hack_debug_enabled = true;
+}
+
+void hack_print(const char *format, ...)
+{
+    if (!hack_debug_enabled)
+        return;
+    char buf[MAXLEN];
+    va_list args;
+    va_start(args, format);
+    bool truncated = vsnprintf(buf, MAXLEN, format, args) > MAXLEN;
+    va_end(args);
 }
--- winsup/cygwin/hack.h
+++ winsup/cygwin/hack.h
@@ -1,6 +1,8 @@
 #ifndef _HACK_H
 #define _HACK_H
 
+extern bool hack_debug_enabled;
 void hack_init(int argc, const char * const * argv);
+void hack_print(const char * format, ...);
 
 #endif
