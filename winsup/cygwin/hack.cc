5ccabf92462b4fa314e3b163c9797ded537a3275
Oskar Skog <oskar@oskog97.com>
2022-08-18 16:32:49 +0300

    Try to open a file

--- winsup/cygwin/hack.cc
+++ winsup/cygwin/hack.cc
@@ -1,15 +1,47 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include "winlean.h"
 
 #include "hack.h"
 #define MAXLEN 1024
 
+// POSIX functions may only be used until hack_debug_enabled is set to true.
 bool hack_debug_enabled = false;
+
+static HANDLE debug_log;
 
 void hack_init(int argc, const char * const * argv)
 {
-    // Proof of concept
-    // fputs("Hello world!\n", stderr);
+    // Get ISO8601 timestamp
+    char timestamp[20];
+    time_t t = time(NULL);
+    struct tm * tmp = gmtime(&t);
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", tmp);
+    // Produce filename
+    char *filename;
+    asprintf(
+        &filename, "C:\\cygdbg\\%s-[%s]-%d.txt",
+        timestamp_s, argv[0], getpid()
+    );
+    // Open log file
+    debug_log = CreateFile(
+                    filename,
+                    GENERIC_WRITE,  // dwDesiredAccess
+                    0,              // dwShareMode      7 for POSIXy
+                    NULL,           // lpSecurityAttribytes
+                    CREATE_ALWAYS,  // dwCreationDisposition
+                    // dwFlagsandAttributes:
+                    FILE_ATTRIBUTE_NORMAL | FILE_FLAG_WRITE_THROUGH,
+                    // hTemplateFile
+                    NULL
+    );
+    free(filename);
+    if (debug_log == INVALID_HANDLE_VALUE) {
+        return;
+    }
+    // Init completed
     hack_debug_enabled = true;
 }
 
@@ -28,4 +60,5 @@ void hack_print(const char *format, ...)
     va_end(args);
     // -Werror for unused variables
     no_op(truncated);
+    // Write to debug_log
 }
