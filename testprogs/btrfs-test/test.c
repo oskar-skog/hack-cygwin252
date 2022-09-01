df13c977705542541fa644f17f2a6698c4d9c06c
Oskar Skog <oskar@oskog97.com>
2022-09-01 23:29:59 +0300

    ...

--- testprogs/btrfs-test/test.c
+++ testprogs/btrfs-test/test.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -6,6 +7,16 @@
 int main()
 {
     struct stat buf;
+    stat("/cygdrive/c", &buf);
+    
+    errno = 0;
     stat("/cygdrive/c/", &buf);
+    
+    errno = 0;
+    stat("/cygdrive/d", &buf);
+    
+    errno = 0;
     stat("/cygdrive/d/", &buf);
+    
+    return 0;
 }
