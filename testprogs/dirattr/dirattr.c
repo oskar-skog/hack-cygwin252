204ee861bb738536c1dae0b2e9fb22572e544059
Oskar Skog <oskar@oskog97.com>
2022-09-06 11:40:45 +0300

    dirattr.c

--- testprogs/dirattr/dirattr.c
+++ testprogs/dirattr/dirattr.c
@@ -1,25 +1,32 @@
+// Native Windows application
+
 /*
 https://stackoverflow.com/questions/10198420/open-directory-using-createfile
 https://docs.microsoft.com/en-us/windows/win32/fileio/file-attribute-constants
 https://docs.microsoft.com/en-us/windows/win32/fileio/obtaining-a-handle-to-a-directory
 */
 
-
 #include <stdio.h>
 #include <windows.h>
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 2) {
+        fprintf(stderr, "Usage: dirattr path\nEx: dirattr C:\\\n");
         return 1;
+    }
     HANDLE h;
     h = CreateFileA(argv[1], 0, 0, NULL, OPEN_EXISTING,
                     FILE_FLAG_BACKUP_SEMANTICS, NULL);
-    if (h == INVALID_HANDLE_VALUE)
+    if (h == INVALID_HANDLE_VALUE) {
+        fprintf(stderr, "Error: could not open specified path\n");
         return 2;
+    }
     FILE_BASIC_INFO info;
-    if (!GetFileInformationByHandleEx(h, FileBasicInfo, &info, sizeof(info)))
+    if (!GetFileInformationByHandleEx(h, FileBasicInfo, &info, sizeof(info))) {
+        fprintf(stderr, "Error: could not get file information\n");
         return 3;
-    printf("0x%x\n", info.FileAttributes);
+    }
+    printf("Attributes: 0x%x\n", info.FileAttributes);
     return 0;
 }
