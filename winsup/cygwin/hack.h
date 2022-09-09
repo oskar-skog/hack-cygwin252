83e4269ebdba708d3109ee123783f9f06e370bc5
Oskar Skog <oskar@oskog97.com>
2022-09-09 16:08:42 +0300

    Typo

--- winsup/cygwin/hack.h
+++ winsup/cygwin/hack.h
@@ -4,7 +4,7 @@
 #define HACK_FORK_ATTEMPTS          50
 #define HACK_DEBUG_FORK             1
 
-// Qll kinds of logging for ReactOS CORE-18247
+// All kinds of logging for ReactOS CORE-18247
 #define HACK_DEBUG_OPEN             1
 #define HACK_DEBUG_STAT             1
 #define HACK_DEBUG_READ             1
