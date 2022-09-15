79d56d4a84e68a7fd717670f7a9687130f831d77
Oskar Skog <oskar@oskog97.com>
2022-09-15 15:30:39 +0300

    ...

--- testprogs/CORE-18248/2.c
+++ testprogs/CORE-18248/2.c
@@ -5,6 +5,8 @@ int main()
     DEL_TESTFILE;
     // Create file for reading and writing
     int fd;
+    printf("Calling open the first time\n");
+    FLUSH;
     fd = open(path, O_RDWR | O_CREAT | O_TRUNC, 0644);
     if (fd < 0) {
         printf("First call to open(2) failed, errno = %d (%s)\n",
@@ -13,6 +15,8 @@ int main()
     }
     close(fd);
     // Open file again
+    printf("Calling open the second time\n");
+    FLUSH;
     fd = open(path, O_RDWR | O_CREAT | O_TRUNC, 0644);
     if (fd < 0) {
         printf("Second call to open(2) failed, errno = %d (%s)\n",
